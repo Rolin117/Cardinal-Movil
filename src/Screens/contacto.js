d89b77cbaea1c25d14a49dcdb18f0d7bb0d695e4
alvaromonterrosa <monterrosaalvaro35qgmail.com>
2024-07-24 18:42:56 -0600

    nueva-pan

--- src/Screens/contacto.js
+++ src/Screens/contacto.js
@@ -17,79 +17,87 @@ export default function home({ navigation }) {
           <Icon name="shopping-cart" size={24} color="black" />
         </TouchableOpacity>
       </View>
-      </View>
-    );
+      <Text style={styles.productName}>Contactanos!</Text>
+      <Text style={styles.contacDescription}>¿Quieres contratar uno de nuestros servicios o tienes una consulta?.
+        Ponte en contacto con nosotros.</Text>
+    </View>
+  );
 }
 
 //Se comienza el código css
 const styles = StyleSheet.create({
-    screen: {
-      flex: 1,
-    },
-    header: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 16,
-      backgroundColor: '#fff',
-      borderBottomWidth: 1,
-      borderBottomColor: '#ccc',
-      paddingTop: 35,
-    },
-    logo: {
-      width: 50,
-      height: 50,
-    },
-    searchInput: {
-      flex: 1,
-      marginLeft: 16,
-      padding: 8,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 8,
-    },
-    cartIcon: {
-      marginLeft: 16,
-    },
-    container: {
-      padding: 16,
-      margin: 20,
-      alignItems: 'center',
-    },
-    card: {
-      width: '100%',
-      marginBottom: 16,
-    },
-    productImage: {
-      height: 150,
-      resizeMode: 'cover',
-    },
-    productName: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginTop: 8,
-    },
-    productPrice: {
-      fontSize: 16,
-      color: '#888',
-      marginVertical: 8,
-    },
-    button: {
-      backgroundColor: '#00B207',
-      padding: 10,
-      borderRadius: 5,
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-    footer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      padding: 16,
-      backgroundColor: '#fff',
-      borderTopWidth: 1,
-      borderTopColor: '#ccc',
-    },
-  });
+  screen: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingTop: 35,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+  },
+  searchInput: {
+    flex: 1,
+    marginLeft: 16,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+  },
+  cartIcon: {
+    marginLeft: 16,
+  },
+  container: {
+    padding: 16,
+    margin: 20,
+    alignItems: 'center',
+  },
+  card: {
+    width: '100%',
+    marginBottom: 16,
+  },
+  productImage: {
+    height: 150,
+    resizeMode: 'cover',
+  },
+  productName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 40,
+  },
+
+  contacDescription: {
+    marginTop: 15,
+  },
+
+  productPrice: {
+    fontSize: 16,
+    color: '#888',
+    marginVertical: 8,
+  },
+  button: {
+    backgroundColor: '#00B207',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 16,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+  },
+});
