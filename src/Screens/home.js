a24d447d098ccdc1ecec2c78541f26ceb9c8662f
alvaromonterrosa <monterrosaalvaro35qgmail.com>
2024-08-06 21:30:25 -0600

    mostrar datos listos

--- src/Screens/home.js
+++ src/Screens/home.js
@@ -1,13 +1,39 @@
-//Importaciones 
+// Importaciones
 
-import React from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, ScrollView, Alert } from 'react-native';
 import { Card } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import detalle_producto from './detalle_producto';
+import { useFocusEffect } from '@react-navigation/native';
+import * as Constantes from '../utils/consantes';
 
-// Contenido de la página principal
 export default function home({ navigation }) {
+  const ip = Constantes.IP;
+  const [dataProductos, setDataProductos] = useState([]);
+
+  const getProductos = async () => {
+    try {
+      // utilizar la direccion IP del servidor y no localhost
+      const response = await fetch(`${ip}/Cardinal_SST-Final/api/services/public/producto.php?action=readAll`, {
+        method: 'GET',
+      });
+
+      const data = await response.json();
+      if (data.status) {
+        setDataProductos(data.dataset);
+      } else {
+        console.log(data);
+        Alert.alert('Error productos', data.error);
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Ocurrió un error al listar los productos');
+    }
+  };
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
   return (
     <View style={styles.screen}>
       <View style={styles.header}>
@@ -18,14 +44,14 @@ export default function home({ navigation }) {
         </TouchableOpacity>
       </View>
       <ScrollView contentContainerStyle={styles.container}>
-        {[1, 2, 3, 4].map((item, index) => (
+        {dataProductos.map((item, index) => (
           <Card key={index} containerStyle={styles.card}>
-            <TouchableOpacity onPress={() => navigation.navigate('detalle_producto')}>
-              <Card.Image style={styles.productImage} source={require('../img/kit.png')} />
+            <TouchableOpacity onPress={() => navigation.navigate('detalle_producto', { id: item.id_producto })}>
+              <Card.Image style={styles.productImage} source={{ uri: item.imagen }} />
               <Card.Divider />
             </TouchableOpacity>
-            <Text style={styles.productName}>Producto {item}</Text>
-            <Text style={styles.productPrice}>$10.00</Text>
+            <Text style={styles.productName}>{item.nombre_producto}</Text>
+            <Text style={styles.productPrice}>${item.precio_producto}</Text>
             <TouchableOpacity style={styles.button} onPress={() => { /* lógica para añadir al carrito */ }}>
               <Text style={styles.buttonText}>Añadir al Carrito</Text>
             </TouchableOpacity>
@@ -50,7 +76,7 @@ export default function home({ navigation }) {
   );
 }
 
-//Se comienza el código css
+// Se comienza el código css
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
